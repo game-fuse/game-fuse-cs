4a81ed3ccb768274cc51bdce364f2f92b0e39d86
Dave Robinson <dave.robinson124@gmail.com>
2024-11-03 12:11:12 -0700

    updating scene to make sure fields are active removing code to create default dave users

--- com.gamefuse.sdk/Samples~/GameFuse Example/FriendsExample/Scripts/UI/SignUpUI.cs
+++ com.gamefuse.sdk/Samples~/GameFuse Example/FriendsExample/Scripts/UI/SignUpUI.cs
@@ -8,8 +8,6 @@ public class SignUpUI : MonoBehaviour
 {
     public event Action<string, string> OnSignUp;
 
-    string _userName, _email, _password;
-
     [SerializeField]
     TMP_InputField _userNameInput, _emailInput, _passwordInput, _resultInput;
 
@@ -18,13 +16,6 @@ public class SignUpUI : MonoBehaviour
 
     void Start()
     {
-        int randomNumber = UnityEngine.Random.Range(0, 1000);
-        _userName = $"dave{randomNumber}";
-        _email = $"dave{randomNumber}@dave.com";
-        _password = "password1234";
-        _userNameInput.text = _userName;
-        _emailInput.text = _email;
-        _passwordInput.text = _password;
         _signUpButton.onClick.AddListener(SignUp);
     }
 
