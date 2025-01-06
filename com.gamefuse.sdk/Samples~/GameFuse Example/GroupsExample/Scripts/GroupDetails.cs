35c5915caf4cfb4d35b7038b90a1168c527c1959
Dave Robinson <dave.robinson124@gmail.com>
2025-01-06 08:39:51 -0700

    adding the ability to list memebers and admins

--- com.gamefuse.sdk/Samples~/GameFuse Example/GroupsExample/Scripts/GroupDetails.cs
+++ com.gamefuse.sdk/Samples~/GameFuse Example/GroupsExample/Scripts/GroupDetails.cs
@@ -1,8 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine.UI;
 using GameFuseCSharp;
-
 public class GroupDetails : MonoBehaviour
 {
     [SerializeField]
@@ -14,10 +14,31 @@ public class GroupDetails : MonoBehaviour
         _maxGroupSizeInput,
         _searchableInput;
 
+    [SerializeField]
+    MemberUI _memberUIPrefab;
+
+    [SerializeField]
+    Transform _contentTransform;
+
+    [SerializeField]
+    TMP_InputField _resultInput;
+
+    [SerializeField]
+    Button memberTabButton, adminTabButton, joinRequestTabButton, invitesTabButton;
+
+    private List<MemberUI> _membersUI = new List<MemberUI>();
+
+    private int _groupId;
 
     public void Awake()
     {
         GroupUI.OnGroupUIClicked += SetValues;
+    }
+
+    private void Start()
+    {
+        memberTabButton.onClick.AddListener(GetGroupMembers);
+        adminTabButton.onClick.AddListener(GetGroupAdmins);
     }
 
     public void OnDestroy()
@@ -27,6 +48,13 @@ public class GroupDetails : MonoBehaviour
 
     public void SetValues(GroupResponse groupResponse)
     {
+        _groupId = groupResponse.Id;
+        SetGroupDetails(groupResponse);
+        ActivateButtons();
+    }
+
+    private void SetGroupDetails(GroupResponse groupResponse)
+    {
         _idInput.text = groupResponse.Id.ToString();
         _nameInput.text = groupResponse.Name;
         _typeInput.text = groupResponse.GroupType;
@@ -35,4 +63,69 @@ public class GroupDetails : MonoBehaviour
         _maxGroupSizeInput.text = groupResponse.MaxGroupSize.ToString();
         _searchableInput.text = groupResponse.Searchable.ToString();
     }
+
+    private void ActivateButtons()
+    {
+        memberTabButton.interactable = true;
+        adminTabButton.interactable = true;
+        joinRequestTabButton.interactable = true;
+        invitesTabButton.interactable = true;
+    }
+
+    
+    private async void GetGroupMembers()
+    {
+        try
+        {
+            GroupResponse groupResponse = await GameFuseUser.CurrentUser.GetGroupDetailsAsync(_groupId);
+            ClearMembers();
+            foreach(var user in groupResponse.Members)
+            {
+                MemberUI memberUI = Instantiate(_memberUIPrefab, _contentTransform);
+                memberUI.transform.SetParent(_contentTransform, false);
+                memberUI.SetUserInfo(user);
+                _membersUI.Add(memberUI);
+            }
+
+        }
+        catch (ApiException ex)
+        {
+            string exceptionMessage = $"Get Group Details Api Exception: \n Status Code: {ex.StatusCode} \n {ex.Message}";
+            Debug.LogError(exceptionMessage);
+            _resultInput.text = exceptionMessage;
+        }
+    }
+
+    private async void GetGroupAdmins()
+    {
+        try
+        {
+            GroupResponse groupResponse = await GameFuseUser.CurrentUser.GetGroupDetailsAsync(_groupId);
+            ClearMembers();
+            foreach (var user in groupResponse.Admins)
+            {
+                MemberUI memberUI = Instantiate(_memberUIPrefab, _contentTransform);
+                memberUI.transform.SetParent(_contentTransform, false);
+                memberUI.SetUserInfo(user);
+                _membersUI.Add(memberUI);
+            }
+
+        }
+        catch (ApiException ex)
+        {
+            string exceptionMessage = $"Get Group Details Api Exception: \n Status Code: {ex.StatusCode} \n {ex.Message}";
+            Debug.LogError(exceptionMessage);
+            _resultInput.text = exceptionMessage;
+        }
+    }
+
+    private void ClearMembers()
+    {
+        foreach(var memberUI in _membersUI)
+        {
+            Destroy(memberUI.gameObject);
+        }
+        _membersUI.Clear();
+    }
+
 }
