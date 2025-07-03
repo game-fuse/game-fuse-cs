1d445294e275708b8a7feb847ccb9b8a2442d614
Dave Robinson <dave.robinson124@gmail.com>
2025-07-03 06:36:31 -0600

    fix for message panel

--- com.gamefuse.sdk/Samples~/GameFuseReferenceUI/Controls/MessagesPanel.cs
+++ com.gamefuse.sdk/Samples~/GameFuseReferenceUI/Controls/MessagesPanel.cs
@@ -368,11 +368,9 @@ namespace GameFuse.UI.Controls
             // Auto-hide after 3 seconds
             var hideTask = System.Threading.Tasks.Task.Delay(3000).ContinueWith(_ =>
             {
-                UnityEditor.EditorApplication.delayCall += () =>
-                {
-                    _statusLabel.style.display = DisplayStyle.None;
-                };
-            });
+                UnityEngine.Application.isPlaying = UnityEngine.Application.isPlaying; // Ensure we're on main thread
+                _statusLabel.style.display = DisplayStyle.None;
+            }, System.Threading.Tasks.TaskScheduler.FromCurrentSynchronizationContext());
         }
         
         public void UpdateChatsDisplay(List<Chat> chats, int currentPage, int totalPages)
